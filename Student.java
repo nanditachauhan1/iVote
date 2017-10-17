18d6f2da46961bd42844d910fd7a41e2abdee53d
nanditachauhan1 <chauhannandita@yahoo.com>
2017-10-16 21:03:12 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,14 +1,6 @@
 package edu.csupomona.cs356.ivote;
 import java.util.List;
 
-/**
-  * A student has a studentID, and an answer. To make things simple my studentID
-  * is just a number which is derived from count which keeps track
-  * of the number of instances created. A student also has answers.
-  * As a student you can enter answers, submit answers, and check
-  * answers.
-  */
-
 public class Student {
 
     private static int count = 0;
