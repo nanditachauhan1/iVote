40b871fb8d7125c57e1329486bcd1550e1f06538
nanditachauhan1 <chauhannandita@yahoo.com>
2017-10-16 21:06:01 -0700

    Update IVote.java

--- IVote.java
+++ IVote.java
@@ -4,20 +4,13 @@ import java.util.List;
 
 public interface IVote {
 
-	/**
-	  * Accept a submission from a student. Validate the submission by
-	  * making sure the answers submitted are in the domain of the choices
-	  * and there are no repeat answers.
-	  */
 	boolean submit(String uuid, List<String> submission);
-
-	/** Print out statistics for this iClicker instance. */
+	
 	String showStats();
 
-	/** Return the total number of submissions */
+
 	Integer totalSubmissions();
 
-	/** Stop accepting submissions */
 	void endSubmissions();
 
 }
