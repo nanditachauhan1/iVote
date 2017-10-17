6b66548ced84c43cd412b165dfd5ab1fa70cbfcc
nanditachauhan1 <chauhannandita@yahoo.com>
2017-10-16 21:04:49 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,12 +1,6 @@
 package edu.csupomona.cs356.ivote;
 import java.util.List;
 
-/**
-  * Abstract class for Single/Multi choice questions
-  * Stores the question in `q`, all the available answer choices
-  * in `choices`, and the correct choices in `correct`
-  */
-
 public abstract class Question {
 
     protected String description;
@@ -27,11 +21,9 @@ public abstract class Question {
         return this.choices;
     }
 
-    /** Make sure a choice is valid */
     public boolean inChoices(String submission) {
         return this.choices.contains(submission);
     }
 
-    /** A prototype method for checking answer(s) */
     public abstract String checkA(List<String> submittedAnswers);
 }
